1a76e993c01aaea4d0d65ba0ea0673cbf1bb4613
Faizan418 <muhammadf4060@gmail.com>
2026-01-18 22:16:48 +0500

    update

--- expense-tracker/src/api.js
+++ expense-tracker/src/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-    // baseURL:"https://expanse-tracker-backend-alpha.vercel.app",
-    baseURL:"http://localhost:5000",
+    baseURL:"https://expanse-tracker-backend-alpha.vercel.app",
+    // baseURL:"http://localhost:5000",
     
     withCredentials: true
 })
